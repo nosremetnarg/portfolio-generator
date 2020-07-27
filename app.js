20fc67591d8b7471312847b7a26482b61d37cfe4
nosremetnarg <grant.alex.emerson@gmail.com>
2020-07-27 17:22:54 -0500

    modular code and html creation

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log("=========");
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-}
-printProfileData(profileDataArgs);
 
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
