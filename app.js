19037073e896a5ba8ab9805090379567620201bb
nosremetnarg <grant.alex.emerson@gmail.com>
2020-07-28 13:52:27 -0500

    html takes in more information

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -127,7 +130,14 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+        // const pageHTML = generatePage(mockData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+
+          console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
     // .then(answers => console.log(answers))
     // .then(promptProject)
@@ -139,13 +149,9 @@ promptUser()
 
 // const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
-
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 // const [name, github] = profileDataArgs;
-
-
 
 // fs.writeFile('index.html', generatePage(name, github), err => {
 //     if (err) throw err;
@@ -158,10 +164,8 @@ promptUser()
 
 // const pageHTML = generatePage(portfolioData);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw new Error(err);
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+
+
 
 
