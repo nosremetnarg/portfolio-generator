45ce5fd2210770b22ae6b1166a2cb3ac76ac50cb
nosremetnarg <grant.alex.emerson@gmail.com>
2020-07-27 21:16:12 -0500

    inquirer installed

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 
+// const fs = require('fs');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const [name, github] = profileDataArgs;
+
+
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
+// const fs = require('fs');
+// const generatePage = require('./src');
+//...
+
+// const pageHTML = generatePage(portfolioData);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
